96b22504ae08ca96cf30537afae09b60813f297f
blulw <brandonlmfaoooo@gmail.com>
2024-06-15 04:19:44 +0100

    ping command

--- cogs/util.py
+++ cogs/util.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-
+from main import bot
 class Util(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -14,11 +14,20 @@ class Util(commands.Cog):
         try:
             embedded_msg = discord.Embed(title="Pong!")
             embedded_msg.add_field(name="Latency", value=f"{round(self.bot.latency * 1000)}ms", inline=True)
-            embedded_msg.set_footer(text=self.bot.user.name, icon_url=self.bot.user.avatar_url)
+            embedded_msg.set_footer(text=self.bot.user.name, icon_url=self.bot.user.avatar)
             await ctx.send(embed=embedded_msg)
         except Exception as e:
             user = await self.bot.fetch_user('817475032112037888')
             await user.send("Exception in ping: ```" + str(e) + "```")
-
+    @bot.tree.command(name="ping", description="displays the ping of the bot")
+    async def ping(self, interaction: discord.Interaction):
+        try:
+            embedded_msg = discord.Embed(title="Pong!")
+            embedded_msg.add_field(name="Latency", value=f"{round(self.bot.latency * 1000)}ms", inline=True)
+            embedded_msg.set_footer(text=self.bot.user.name, icon_url=self.bot.user.avatar)
+            await interaction.response.send_message(embed=embedded_msg)
+        except Exception as e:
+            user = await self.bot.fetch_user('817475032112037888')
+            await user.send("Exception in ping: ```" + str(e) + "```")
 async def setup(bot):
     await bot.add_cog(Util(bot))
